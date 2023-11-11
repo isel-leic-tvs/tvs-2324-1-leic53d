d3a4062b52b46977ae37eba05ff2d7a3df7969ae
João Trindade <joao.trindade@isel.pt>
2023-11-11 21:07:23 +0000

    Lecture 22 (fix)

--- 10-systemd/01-service/useless.sh
+++ 10-systemd/01-service/useless.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-echo "This is a useless service"
+echo "This is a useless service."
+
+function handleTerm() {
+	echo "I refuse to stop being useless!"
+}
+
+trap handleTerm TERM
 
 while :
 do
 	sleep 8
-	echo "Still here, being slightly more useless than a TVS prof."
+	echo "Still useless..."
 done
+
