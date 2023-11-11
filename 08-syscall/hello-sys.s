84a1b49b697c8ae33b0ac8353becd699cc1b445c
João Trindade <joao.trindade@isel.pt>
2023-11-11 21:06:26 +0000

    Lecture 10 (fix)

--- 08-syscall/hello-sys.s
+++ 08-syscall/hello-sys.s
@@ -6,16 +6,14 @@ _start:
 	movq $14, %rdx
 	leaq msg(%rip), %rsi
 	movq $1, %rdi
-
 	movq $1, %rax
 	syscall
 	
 	# exit(0);
 	xorq %rdi, %rdi
-	
-	mov $60, %rax
+	movq $60, %rax
 	syscall
 	
-msg: 	.ascii "Hello, world!\n"	
+msg: .ascii "Hello, world!\n"	
 
 	.end
